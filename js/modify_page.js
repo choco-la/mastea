7bc0ccc2cb58237ae1ebc45929c10b1866959492
hyoro-were <choco-la@users.noreply.github.com>
2019-04-30 08:13:34 +0900

    add modify_page.js

---
+++ js/modify_page.js
@@ -0,0 +1,5 @@
+/* Modify Page DOM */
+(() => {
+  const floatingActionButton = document.querySelector('.floating-action-button')
+  if (floatingActionButton) floatingActionButton.style.display = 'none'
+})()
